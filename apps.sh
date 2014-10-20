1478f0fb247b51695463cdb7f714e7b3f2d699ed
Ben Ehmke <benehmke@gmail.com>
2014-10-20 17:17:38 -0500

    include lots of fonts

--- apps.sh
+++ apps.sh
@@ -34,7 +34,52 @@ brew tap caskroom/fonts
 fonts=(
   font-m-plus
   font-clear-sans
+  font-bebas-neue
+  font-arvo
+  font-bilbo
+  font-cabin
+  font-coda
+  font-droid-sans
+  font-fasthand
+  font-freehand
+  font-geo
+  font-geo-sans-light
+  font-just-another-hand
+  font-lato
+  font-league-gothic
+  font-lemon
+  font-lobster
+  font-lobster-two
+  font-m-plus
+  font-marvel
+  font-maven-pro
+  font-nothing-you-could-do
+  font-nova-cut
+  font-nova-oval
+  font-nova-round
+  font-nova-flat
+  font-nova-script
+  font-nova-slim
+  font-nova-square
+  font-open-sans
+  font-oswald
+  font-over-the-rainbow
+  font-pacifico
+  font-patrick-hand
+  font-patrick-hand-sc
+  font-prime
+  font-pt-mono
+  font-pt-sans
+  font-pt-serif
+  font-quicksand
+  font-racing-sans-one
+  font-raleway
+  font-raleway-dots
   font-roboto
+  font-roboto-slab
+  font-verdana
+  font-wendy-one
+  font-yanone-kaffeesatz
 )
 
 # install fonts
